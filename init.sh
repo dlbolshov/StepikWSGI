c06234492533d727626f1a372417a58cf57ac8e3
dlbolshov <dlbolshov@gmail.com>
2022-07-06 02:48:06 +0300

    fix import error

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/
+sudo rm -rf /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:hello  # --bind='ip-адрес:порт'
+gunicorn --bind='0.0.0.0:8080' hello:hello  # -b 0.0.0.0:8080
