b50ed16c9a3fdbb14dbdf5ed4c7cdc6baeddddf7
dlbolshov <dlbolshov@gmail.com>
2022-07-06 03:01:45 +0300

    stupid task

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
 sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/
-sudo rm -rf /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-gunicorn --bind='0.0.0.0:8080' hello:hello  # -b 0.0.0.0:8080
